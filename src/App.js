eaba881509ac05f09e506bf7a27b396e517241aa
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2024-01-01 12:06:12 +0100

    change router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from "./components/Home/Home";
-import { Route, Router } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import classes from "./App.module.css";
@@ -7,11 +7,11 @@ import classes from "./App.module.css";
 const App = () => {
   return (
     <div className={classes.pageContainer}>
-      <Router basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-      </Router>
+      </HashRouter>
     </div>
   );
 };
